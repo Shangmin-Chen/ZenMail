661f6de899629d8c98b3b55c7d192b5ebec89c68
Shangmin <shangminch@gmail.com>
2025-11-18 15:26:53 -0500

    Created project structure and depdencies

---
+++ src-tauri/src/handlers/mod.rs
@@ -0,0 +1,3 @@
+pub mod imap;
+pub mod smtp;
+pub mod account;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod handlers;
+mod models;
 
 fn main() {
     zenmail_lib::run()
